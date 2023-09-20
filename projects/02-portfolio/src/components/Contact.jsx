198e92de6661a304bc7839fbb0983a2dfe1c5c23
Jhon Henao <henao.jhon@correounivalle.edu.co>
2023-09-20 10:41:18 -0500

    Now is ready to be deployed

--- projects/02-portfolio/src/components/Contact.jsx
+++ projects/02-portfolio/src/components/Contact.jsx
@@ -1,11 +1,12 @@
 
 export default function Contact () {
  return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col text-center gap-4">
         <h1 className="text-center">Contact me</h1>
         <p className="text-center">
             Contact me or observe my work through the following social networks and repositories !!
         </p>
+        <h3>To highlight, this portfolio is made by React, Vite and extracting data from github api</h3>
         <section className="flex gap-4 justify-center">
             <a href="https://www.linkedin.com/in/jhon-henao-abb272196/">
                 <ion-icon name="logo-linkedin">
