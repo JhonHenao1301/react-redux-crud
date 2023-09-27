6705636ccb226c7010d77cee2670d3ce188a24cf
Jhon Henao <henao.jhon@correounivalle.edu.co>
2023-09-27 07:42:46 -0500

    Deleting midu project

--- projects/01-crud-react-redux/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from "./users/slice";
-
-export const store = configureStore({
-    reducer: {
-        users: usersReducer,
-    },
-})
