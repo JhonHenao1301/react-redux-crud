050525b8d8dd6d8298235d3975f966feda62ca03
Jhon Henao <henao.jhon@correounivalle.edu.co>
2023-09-26 20:07:48 -0500

    Adding store function

--- projects/01-crud-react-redux/src/components/LIstOfUsers.tsx
+++ projects/01-crud-react-redux/src/components/LIstOfUsers.tsx
@@ -6,61 +6,20 @@ import {
     TableHead,
     TableHeaderCell,
     TableBody,
+    Title,
+    Badge
   } from "@tremor/react";
-  
-  const salesPeople: {
-    id: string;
-    name: string;
-    email: string;
-    github: string;
-  }[] = [
-    {
-      id: "1",
-      name: "Jose Rodriguez",
-      email: "j.rodriguez@gmail.com",
-      github: "JRODRIGUEZ",
-    },
-    {
-      id: "2",
-      name: "Mario Rodriguez",
-      email: "m.rodriguez@gmail.com",
-      github: "MRODRIGUEZ",
-    },
-    {
-      id: "3",
-      name: "Isabella Henao",
-      email: "i.henao@gmail.com",
-      github: "IHENAO",
-    },
-    {
-      id: "4",
-      name: "Mario Araujo",
-      email: "m.araujo@gamail.com",
-      github: "MARAUJO",
-    },
-    {
-      id: "5",
-      name: "Sebastian Tamayo",
-      email: "s.tamayo@gmail.com",
-      github: "STAMAYO",
-    },
-    {
-      id: "6",
-      name: "James Londono",
-      email: "j.londono@gmail.com",
-      github: "JLONDONO",
-    },
-    {
-      id: "7",
-      name: "Jhon Henao",
-      email: "henao1301@gmail.com",
-      github: "JHONHENAO1301",
-    },
-  ];
+
+  import { useSelector } from "react-redux";
   
   export default function ListOfUsers() {
+    const users = useSelector((state) => state.users);
     return (
       <Card>
+        <Title className="flex gap-2">
+          Users
+          <Badge>{users.length}</Badge>
+        </Title>
         <Table>
           <TableHead>
             <TableRow>
@@ -72,7 +31,7 @@ import {
           </TableHead>
   
           <TableBody>
-            {salesPeople.map((item) => (
+            {users.map((item) => (
               <TableRow key={item.name}>
                 <TableCell>{item.id}</TableCell>
                 <TableCell className="flex gap-4 items-center">
@@ -80,8 +39,17 @@ import {
                     {item.name}
                 </TableCell>
                 <TableCell>{item.email}</TableCell>
-                <TableCell>
-                    Actions
+                <TableCell className="flex gap-2">
+                    <button type="button">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                      </svg>
+                    </button>
+                    <button type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                    </button>
                 </TableCell>
               </TableRow>
             ))}
--- projects/01-crud-react-redux/src/main.tsx
+++ projects/01-crud-react-redux/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { store } from './store/index.ts'
+import { Provider } from 'react-redux'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
