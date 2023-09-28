f726c4387066348f399c2e4ac4d4a0df91e15bb2
Jhon Henao <henao.jhon@correounivalle.edu.co>
2023-09-28 11:31:12 -0500

    Links redirected to another tab in contact component

--- projects/02-portfolio/src/components/Contact.jsx
+++ projects/02-portfolio/src/components/Contact.jsx
@@ -8,13 +8,13 @@ export default function Contact () {
         </p>
         <h3>To highlight, this portfolio is made by React, Vite and extracting data from github api</h3>
         <section className="flex gap-4 justify-center">
-            <a href="https://www.linkedin.com/in/jhon-henao-abb272196/">
+            <a href="https://www.linkedin.com/in/jhon-edinson-henao-abb272196/" target="_blank">
                 <ion-icon name="logo-linkedin">
                 </ion-icon>
             </a>
-            <a href="https://github.com/JhonHenao1301?tab=repositories">
+            <a href="https://github.com/JhonHenao1301?tab=repositories" target="_blank">
                 <ion-icon name="logo-github">
-                </ion-icon>
+                </ion-icon> 
             </a>
         </section>
     </div>
